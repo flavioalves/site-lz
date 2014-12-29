c72d19e0cdfe0c1d18f2a54d3af75cfe3964ef5f
Flavio Alves <flaviof.alves@gmail.com>
2014-12-28 23:26:49 -0200

    home responsive

--- app/assets/javascripts/slider-home.js
+++ app/assets/javascripts/slider-home.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   //fit page on screen
   var sections = $(".slider-container");
   var height = $(window).height();
-  $(".slider-container ul.bxslider li.item").height(height);
+  $(".slider-container ul.bxslider li.item img.bg").height(height);
 
 
   var slider = $('.bxslider').bxSlider({
