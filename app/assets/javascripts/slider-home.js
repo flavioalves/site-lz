cf28d7cb83d51cfd61e2f3bd3581f7602cb0d431
Flavio Alves <flaviof.alves@gmail.com>
2015-01-12 02:00:16 -0200

    improves in style

--- app/assets/javascripts/slider-home.js
+++ app/assets/javascripts/slider-home.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
 
+  var height = $(window).height();
+  var width = $(window).width();
+
+  $(".royalSlider").royalSlider({
+      keyboardNavEnabled: true,
+      autoScaleSlider: true,
+      autoScaleSliderHeight: height,
+      autoScaleSliderWidth: width,
+      imageScaleMode: 'fill',
+      center: false
+  });  
+ 
+
+  /*
   //fit page on screen
   var sections = $(".slider-container");
   var height = $(window).height();
   $(".slider-container ul.bxslider li.item img.bg").height(height);
 
-
   var slider = $('.bxslider').bxSlider({
     adaptiveHeight: true
   });
   //slider.startAuto();
+  */
 });
