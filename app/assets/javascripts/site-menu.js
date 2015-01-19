9a3d8e3d3f8d6aed9272db40c0a047065d102c04
Flavio Alves <flaviof.alves@gmail.com>
2015-01-19 21:30:53 -0200

    improve header loading experience

--- app/assets/javascripts/site-menu.js
+++ app/assets/javascripts/site-menu.js
@@ -21,29 +21,5 @@ $(document).ready(function(){
   }); 
 
 
-
-  $('.main-menu li').click(function(e){
-    $('.main-menu').find('li.active').removeClass('active');
-    $(e.currentTarget).addClass('active');    
-  });
-  
-  //set menu location
-  var pathArray = window.location.pathname.split( '/' );
-  var path = pathArray[1];
-
-  var menuItens = $('.main-menu li');
-  $(menuItens).each(function(){
-    //clean old selection
-    $(this).removeClass('active');
-
-    if(path && path.length > 1){
-      if($(this).find('a').attr('href') == "/" + path){
-        $(this).addClass('active');        
-      }
-    } else {
-      //is in home page remove yellow bord on footer. improve it.      
-      $("#footer").addClass("yellow-border");
-    }
-  });  
-  
+  $('.header-container').fadeIn( "slow" );  
 });
