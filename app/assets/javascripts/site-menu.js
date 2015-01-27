71771cdda55f4ed265fd6d33898d73fb5edb01ff
Flavio Alves <flaviof.alves@gmail.com>
2015-01-27 17:59:03 -0200

    improve scroll experience

--- app/assets/javascripts/site-menu.js
+++ app/assets/javascripts/site-menu.js
@@ -12,17 +12,61 @@ $(document).ready(function(){
       }
     });  
 
+  $("#site-menu-bar").mmenu({
+      classes     : 'mm-white',
+      counters    : true,
+      offCanvas : {
+        position: "right"
+      }
+    },{
+      offCanvas: {
+        pageNodetype: "section.page"
+      }
+  });
+
   // controll header
-  var myHeader = document.querySelector("header");
-  var headroom  = new Headroom(myHeader);
-
-  headroom.init({
+  $("#header").headroom({
     offset : 200,
-    tolerance : 200,
-  }); 
+    tolerance : 5,
+  });
+  
+  var offset = 10;
+  var heightToTop = $(window).scrollTop();
+  if(heightToTop < offset){
+    $('.header-top-container').fadeIn( "slow" );  
+  }
 
+  // var lastScrollTop = 0;
+  // $(window).scroll(function(event){
+  //    var st = $(this).scrollTop();
+  //    var isHeaderBarOpen = false;
+  //    var isHeaderTopOpen = false;
 
-  $('.header-container').fadeIn( "slow" );  
+  //    if (st > lastScrollTop && st > offset){
+  //       $('.header-top-container').fadeOut( "slow", function(){
+  //         $(this).hide();
+  //       });  
+  //    } else {
+  //       // upscroll code
+  //       if(st <= offset){
+  //         $('.header-bar-container').fadeOut( "slow", function(){
+  //           $(this).hide();
+  //         }); 
+
+  //         $('.header-top-container').fadeOut( "slow", function(){
+  //           $(this).show();
+  //         });
+
+  //       } else {
+  //         $('.header-bar-container').fadeIn( "slow", function(){
+  //           $(this).show();
+  //         }); 
+  //       }
+  //    }
+  //    lastScrollTop = st;
+  // });  
+
+  
 
   
 });
