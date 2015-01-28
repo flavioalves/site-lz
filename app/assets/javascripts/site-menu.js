dae4a41eff83ce12a1c2790c3949ba958c75c01c
Flavio Alves <flaviof.alves@gmail.com>
2015-01-28 16:20:33 -0200

    improve socios images

--- app/assets/javascripts/site-menu.js
+++ app/assets/javascripts/site-menu.js
@@ -35,38 +35,5 @@ $(document).ready(function(){
   if(heightToTop < offset){
     $('.header-top-container').fadeIn( "slow" );  
   }
-
-  // var lastScrollTop = 0;
-  // $(window).scroll(function(event){
-  //    var st = $(this).scrollTop();
-  //    var isHeaderBarOpen = false;
-  //    var isHeaderTopOpen = false;
-
-  //    if (st > lastScrollTop && st > offset){
-  //       $('.header-top-container').fadeOut( "slow", function(){
-  //         $(this).hide();
-  //       });  
-  //    } else {
-  //       // upscroll code
-  //       if(st <= offset){
-  //         $('.header-bar-container').fadeOut( "slow", function(){
-  //           $(this).hide();
-  //         }); 
-
-  //         $('.header-top-container').fadeOut( "slow", function(){
-  //           $(this).show();
-  //         });
-
-  //       } else {
-  //         $('.header-bar-container').fadeIn( "slow", function(){
-  //           $(this).show();
-  //         }); 
-  //       }
-  //    }
-  //    lastScrollTop = st;
-  // });  
-
-  
-
   
 });
