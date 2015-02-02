bdaa5046a6318c7487d9ddfa65f8b332cd7b5bed
Carolina Mascarenhas <anylyne@gmail.com>
2015-02-02 10:11:57 -0300

    models revised

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,18 @@
 class Client < ActiveRecord::Base
+	include FriendlyId
+	friendly_id :name, use: [:slugged]
+
 	has_many :users
 	has_many :projects
 	has_many :portfolio_projects
 
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
-
+	validates :slug, presence: true
+	
 	# Setup accessible (or protected) attributes for your model
 	attr_accessible :name
+
+	def should_generate_new_friendly_id?
+		name_changed? || super
+	end
 end
