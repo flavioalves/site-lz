b228aab2cb067e5fb810c1536840f803279de390
Carolina Mascarenhas <anylyne@gmail.com>
2015-02-05 11:31:59 -0300

    changing bucket_name in refile configurations.

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ if Rails.env.production? or Rails.env.staging?
 	aws = {
 	  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
 	  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-	  bucket: ENV['S3_BUCKET_NAME']
+	  bucket_name: ENV['S3_BUCKET_NAME']
 	}
 	Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
 	Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
