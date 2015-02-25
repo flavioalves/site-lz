04930bd01073b7e150126d91315a5936af42744d
Carolina Mascarenhas <anylyne@gmail.com>
2015-02-25 16:15:51 -0300

    adding CloudFront Distributions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ SiteLZ::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   config.action_controller.asset_host = "dluafhj7422d4.cloudfront.net"
+  
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
