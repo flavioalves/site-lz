4c84dca7f03ab7dc247a3009e5852b9bbd0144a6
Helio Adson <helioadson@gmail.com>
2021-09-10 19:38:37 -0300

    asset compile for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ SiteLZ::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # config.assets.compile = false
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
