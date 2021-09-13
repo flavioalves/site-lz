e540a1712b1a18b16807ef00342e360699ea774b
Helio Adson <helioadson@gmail.com>
2021-09-13 12:05:16 -0300

    setting asset compile to false in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 SiteLZ::Application.configure do
   Slim::Engine.set_default_options :format => :html5
   # Settings specified here will take precedence over those in config/application.rb
-  
+
   config.eager_load = true
   config.assets.js_compressor = :uglifier
 
@@ -18,11 +18,11 @@ SiteLZ::Application.configure do
   config.static_cache_control = 'public, max-age=31536000'
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # config.assets.compile = false
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
